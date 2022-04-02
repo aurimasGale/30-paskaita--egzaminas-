fda2a60ef5db7df70be08a79a4731ae3366906ed
aurimasGale <you@example.com>
2022-04-02 14:43:01 +0300

    done without css

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,51 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+const formEl = document.forms[0];
+const divEl = document.getElementById("output");
+const inputEl = document.getElementById("search");
+const ulEl = document.createElement("ul");
+const li1El = document.createElement("li");
+const li2El = document.createElement("li");
+const li3El = document.createElement("li");
+
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  console.log(inputEl.value);
+  const parsed = parseInt(inputEl.value);
+  console.log(parsed);
+  if (parsed == inputEl.value) {
+    kgConversion(parsed);
+  } else {
+    showFailAlert();
+    setTimeout(() => {
+      location.reload();
+    }, 2000);
+  }
+
+  function kgConversion(parsed) {
+    const toLb = parsed * 2.2046;
+    const toG = parsed * 1000;
+    const toOz = parsed * 35.274;
+    console.log(toLb);
+
+    ulEl.textContent = `${parsed} kg are equal to:`;
+    li1El.textContent = `${toLb} pounds.`;
+    li2El.textContent = `${toG} grams.`;
+    li3El.textContent = `${toOz} ounces.`;
+    console.log(li1El);
+
+    ulEl.append(li1El, li2El, li3El);
+    divEl.append(ulEl);
+  }
+
+  function showFailAlert() {
+    divEl.textContent = "Please enter a number";
+    divEl.style.textAlign = "center";
+    divEl.style.color = "tomato";
+    setTimeout(() => {
+      divEl.remove();
+    }, 2000);
+  }
+});
