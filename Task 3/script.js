8479d2a559d71d99b50ed4ec6f42fc5df86872f7
aurimasGale <you@example.com>
2022-04-02 17:36:06 +0300

    task 3 working without button

--- Task 3/script.js
+++ Task 3/script.js
@@ -10,4 +10,37 @@ Paspaudus mygtuką "Show users":
 Pastaba: Informacija apie user'į (jo kortelė) bei turi turėti bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'https://api.github.com/users';
+const ENDPOINT = "https://api.github.com/users";
+
+const contEl = document.getElementById("output");
+const btnEl = document.getElementById("btn");
+const divEl = document.createElement("div");
+divEl.className = "container";
+
+async function getProperties() {
+  const res = await fetch(ENDPOINT);
+  const dataArr = await res.json();
+  console.log("data ===", dataArr);
+  renderCards(dataArr);
+}
+// getProperties();
+
+function renderCards(arr) {
+  arr.forEach((arrObj) => {
+    const cardEl = document.createElement("div");
+    cardEl.className = "single-card";
+
+    const imgEl = document.createElement("img");
+    imgEl.src = arrObj.avatar_url;
+
+    const loginEl = document.createElement("p");
+    loginEl.className = "card-text";
+    loginEl.textContent = arrObj.login;
+
+    cardEl.append(imgEl);
+    cardEl.append(loginEl);
+
+    contEl.append(cardEl);
+  });
+}
+// btnEl.addEventListener('click',getProperties() )
