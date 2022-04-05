43ddb32c89ff1be2bc752939f1f119ffd7d24407
aurimasGale <you@example.com>
2022-04-05 14:45:58 +0300

    task 10 done

--- Task 10/script.js
+++ Task 10/script.js
@@ -2,12 +2,21 @@
 Sutvarkykite užduoties "Task 10" esančius failus taip, kad veiktų žemiau pateiktos funcijos
 -------------------------------------------------------------------------------------------- */
 
+import composition from "./modules/math/composition.js";
+import division from "./modules/math/division.js";
+import substraction from "./modules/math/subtraction.js";
+import * as mltp from "./modules/math/multiplication.js";
+
+import { one, two, three, four, five } from "./modules/numbers/numbers.js";
+
 let a = composition(one, four);
 let b = division(four, two);
 let c = substraction(three, two);
-let d = multiplication(five, two);
+let d = mltp.multiplication(five, two);
 
-console.log(a);
-console.log(b);
-console.log(c);
-console.log(c);
+console.log("composition ===", a);
+console.log("division ===", b);
+console.log("substraction ===", c);
+console.log("multiplication ===", d);
+
+// console.log(`multiplication of ${five} and ${two} is ${d}`);
